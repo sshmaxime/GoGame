c1c73758ec6ca172d03d4fbfd8494c6d70ea0f14
Maxime Aubanel <maximeauba@gmail.com>
2020-09-08 11:14:02 +0200

    update url prod

--- client/src/store/actions/websocket.actions.tsx
+++ client/src/store/actions/websocket.actions.tsx
@@ -9,7 +9,7 @@ import { LoginRequest, CreateRoomRequest, LeaveRoomRequest, JoinRoomRequest, Mes
 import { LOGIN_SUCCESS, LEAVE_ROOM_SUCCESS, MESSAGE_ROOM, CREATE_ROOM_SUCCESS, JOIN_ROOM_SUCCESS, READY, UPDATE_STATE } from "../reducers/websocket.reducer";
 import NotificationCenter from '../../global/notification';
 
-const ENDPOINT = "https://localhost:3001";
+const ENDPOINT = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://server.gogame.sshsupreme.xyz";
 
 const socket = io(ENDPOINT, {
   transports: ['websocket'],
